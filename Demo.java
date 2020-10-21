04ef44a698733e40c3ade5b4a9ae2cfdd9b31cfb
Andrew <ajuang20@stuy.edu>
2020-10-21 15:10:57 -0400

    create2dArray method works

--- Demo.java
+++ Demo.java
@@ -1,4 +1,29 @@
 public class Demo{
+    public static void main(String args[]){
+        int[] a = {1};
+        int[] b = {1,2};
+        int[] c = {1,2,3};
+        int[] d = {1,2,3,4};
+        int[] e = {1,2,3,4,5};
+        int[][] test = new int[5][];
+        test[0] = a;
+        test[1] = b;
+        test[2] = c;
+        test[3] = d;
+        test[4] = e;
+        System.out.println(arrToString(a));
+        System.out.println(arrDeepToString(test).replace("}, ","},\n "));
+        System.out.println(arrDeepToString(create2dArray(4,4,4)));
+    }
+    public static int[][] create2dArray(int rows, int cols, int maxValue){
+        int[][] arr = new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j] = (int)Math.floor(Math.random()*(maxValue+1));
+            }
+        }
+        return arr;
+    }
     public static void printLoop(int n){
         for(int i=1;i<=n;i++){
             for(int j=0;j<=n-i;j++){
@@ -7,11 +32,27 @@ public class Demo{
             System.out.print("\n");
         }
     }
-    public static void main(String args[]){
-        if(args.length==0){
-            printLoop(5);
-        } else {
-            printLoop(Integer.parseInt(args[0]));
+    public static String arrToString(int[] arr){
+        String string = "{";
+        for(int i=0;i<arr.length-1;i++){
+            string += arr[i];
+            string += ", ";
         }
+        if(arr.length>=1){
+            string += arr[arr.length-1];
+        }
+        string += "}";
+        return string;
+    }
+    public static String arrDeepToString(int[][] arr){
+        String string = "{";
+        for(int i=0;i<arr.length;i++){
+            string += arrToString(arr[i]);
+            if(i!=arr.length-1){
+                string += ", ";
+            }
+        }
+        string += "}";
+        return string;
     }
 }
